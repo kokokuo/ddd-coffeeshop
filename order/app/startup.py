f7bef45c729f1a3dd343a86065f524ddf325c424
kokokuo <v6610688@gmail.com>
2019-11-10 14:47:16 +0800

    Feature: 1.Fix entity module name 2. change order of code in AppStartup

--- order/app/startup.py
+++ order/app/startup.py
@@ -10,8 +10,8 @@ class AppStartup(object):
     def __init__(self, config: Config):
         self._app = Flask(__name__)
         self._app.config.from_object(config)
-        self._load_router(self._app)
         dbo.init_app(self._app)
+        self._load_router(self._app)
 
     def _load_router(self, app: Flask):
         app.register_blueprint(order.blueprint)
